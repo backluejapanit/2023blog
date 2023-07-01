3d7f24c434119bd410770bd8f00ba3af94c79a0d
backluejapanit <backluejapanit@gmail.com>
2023-07-01 10:47:47 +0900

    add change
    add change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import * as firebaseAdmin from 'firebase-admin'
-import * as serviceAccount from '../post-images-storage-firebase-adminsdk-7r152-efa03de64d.json'
+import * as firebaseAdmin from 'firebase-admin';
+// import * as serviceAccount from '../post-images-storage-firebase-adminsdk-7r152-efa03de64d.json'
 
+const rememberUser: JSON = <JSON>(<unknown>{
+  project_id: '1',
+  private_key: '1',
+  client_email: 'backluejapanit@gmail.com',
+});
 
 export const admin = firebaseAdmin.initializeApp({
   // @ts-ignore
-  credential: firebaseAdmin.credential.cert(serviceAccount)
-})
-export const storageRef = admin.storage().bucket('gs://post-images-storage.appspot.com')
+  credential: firebaseAdmin.credential.cert(rememberUser),
+});
+export const storageRef = admin
+  .storage()
+  .bucket('gs://post-images-storage.appspot.com');
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 5000
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({credentials: true, origin: process.env.CLIENT_URL});
+  app.enableCors({ credentials: true, origin: process.env.CLIENT_URL });
 
   await app.listen(PORT, () => console.log(`Server running om port: ${PORT}`));
 }
